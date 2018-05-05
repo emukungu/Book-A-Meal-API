9b2a9b2806b53669d7e477994ae6286633a4ec75
Esther <enmukungu@gmail.com>
2018-05-05 06:10:47 +0300

    altering the received data

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -17,7 +17,6 @@ class Menu(unittest.TestCase):
 
     def test_get_empty_menu(self):
         result = self.test.get("/api/v1/menu")
-        #print(result.data)
         self.assertEqual(result.status_code, 204) 
 
     def test_set_the_menu(self):
@@ -25,11 +24,13 @@ class Menu(unittest.TestCase):
             "/api/v1/menu", 
             data = json.dumps({"menu_title":"Italian", "menu_items":["beef", "goat", "chicken"]}), 
             content_type = "application/json"
-        )   
+        )
+        print(result.data)   
         self.assertEqual(result.status_code, 200)
 
     def test_get_menu(self):
         result = self.test.get("/api/v1/menu")
+        print(result.data)
         self.assertEqual(result.status_code, 200)
 
 if __name__ == "__main__":
